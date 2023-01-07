06dca6ef69e9029ba05f5af56ad7ba0716dac99d
nachtwandler <ivanlogvynenko2112@gmail.com>
2023-01-07 19:39:38 +0100

    init

---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,18 @@
+//
+// Created by nachtwandler on 07.01.23.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_BINARYTREE_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_BINARYTREE_H
+
+template <class T>
+class BinaryTree {
+private:
+    T* startNode;
+public:
+    BinaryTree();
+    BinaryTree(T* startNode);
+};
+
+
+#endif //DATA_STRUCTURES_AND_ALGORITHMS_BINARYTREE_H
